56933fdbec4763d146839fcab0eaa708a3a86f3d
AuGuSt047 <89444871+AuGuSt047@users.noreply.github.com>
2021-12-21 09:09:13 +0700

    Update province.php

--- province.php
+++ province.php
@@ -1,14 +1,14 @@
 <?php
 
-  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces%22);
   $data = json_decode($json);
-  
+
   echo "<table border='1'>";
   echo "<tr><td>ลำดับ</td><td>จังหวัด</td></tr>";
   foreach($data as $key=>$val){
     echo "<tr>";
     echo "<td>";
-    echo $key;
+    echo ($key+1);
     echo "</td>";
     echo "<td>";
     echo $val->province;
