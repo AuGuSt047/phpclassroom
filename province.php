747e6ac084b9aa82cfc8559201903f6ad6c521bc
AuGuSt047 <89444871+AuGuSt047@users.noreply.github.com>
2021-12-21 09:24:15 +0700

    Update province.php

--- province.php
+++ province.php
@@ -1,9 +1,11 @@
 <?php
 
-  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces%22);
   $data = json_decode($json);
- echo "<table border='1'>";
- echo "<tr><td>ลำดับ</td><td>จังหวัด</td><td>จำนวนผู้ป่วยใหม่</td><td>จำนวนผู้ป่วยสะสม</td>
+
+
+echo "<table border='1'>";
+echo "<tr><td>ลำดับ</td><td>จังหวัด</td><td>จำนวนผู้ป่วยใหม่</td><td>จำนวนผู้ป่วยสะสม</td>
        <td>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td><td>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
        <td>จำนวนผู้เสียชีวิต</td><td>จำนวนผู้เสียชีวิตสะสม</td></tr>";
 
@@ -35,5 +37,6 @@ foreach($data as $key=>$val){
   echo "</td>";
   echo "</tr>";
 }
+echo "</table>";
 
 ?>
