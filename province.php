044f303955a5601bfe612867deb8d6d41091c6b9
AuGuSt047 <89444871+AuGuSt047@users.noreply.github.com>
2021-12-21 10:11:10 +0700

    Update province.php

--- province.php
+++ province.php
@@ -3,11 +3,11 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
 echo "<h1><center><b>รายงานสถานการณ์โควิด-19 แยกจังหวัด</b></center></h1>.'<br>'";
-echo '<body style="background-color:mediumaquamarine">';
+
 echo "<center><table border='1'>";
-echo "<tr><td>ลำดับ</td><td>จังหวัด</td><td>จำนวนผู้ป่วยใหม่</td><td>จำนวนผู้ป่วยสะสม</td>
-       <td>จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td><td>จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
-       <td>จำนวนผู้เสียชีวิต</td><td>จำนวนผู้เสียชีวิตสะสม</td></tr>";
+echo "<tr><td style="background-color:mediumaquamarine">ลำดับ</td><td style="background-color:mediumaquamarine">จังหวัด</td><td style="background-color:mediumaquamarine">จำนวนผู้ป่วยใหม่</td><tdstyle="background-color:mediumaquamarine">จำนวนผู้ป่วยสะสม</td>
+       <tdstyle="background-color:mediumaquamarine">จำนวนผู้ป่วยใหม่(รวมต่างประเทศ)</td><tdstyle="background-color:mediumaquamarine">จำนวนผู้ป่วยสะสม(รวมต่างประเทศ)</td>
+       <tdstyle="background-color:mediumaquamarine">จำนวนผู้เสียชีวิต</td><tdstyle="background-color:mediumaquamarine">จำนวนผู้เสียชีวิตสะสม</td></tr>";
   
 foreach($data as $key=>$val){
   echo "<tr>";
