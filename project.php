317dbee1c8063db095ebe0b5e439841d98f56205
AuGuSt047 <89444871+AuGuSt047@users.noreply.github.com>
2022-01-18 10:30:06 +0700

    Update project.php

--- project.php
+++ project.php
@@ -137,36 +137,33 @@
           </tr>
           <!-- แก้ไขส่วนที่ 3 -->
           <!-- Loop -->
-           
+          <?php
+      $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+        $data = json_decode($json);     
 foreach($data as $key=>$val){
   echo "<tr>";
-  echo "<td style= 'background-color:#F5F5DC'>";
+  echo "<td>";
   echo ($key+1);
   echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
+  echo "<td>";
   echo $val->province;
   echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
+  echo "<td>";
   echo $val->new_case;
   echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
+  echo "<td>";
+  echo $val->new_recovered;
+  echo "</td>";
+  echo "<td>";
   echo $val->total_case;
   echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
-  echo $val->new_case_excludeabroad;
-  echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
-  echo $val->total_case_excludeabroad;
-  echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
+  echo "<td>";
   echo $val->new_death;
   echo "</td>";
-  echo "<td style= 'background-color:#F5F5DC'>";
-  echo $val->total_death;
-  echo "</td>";
+ 
   echo "</tr>";
 }
-
+?>
 Message #work
 }    </table>
       </div>
